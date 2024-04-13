bda4174f985dbc78cdbe7add007b3cd1a533ce7a
yichien <kevinyichienlee@gmail.com>
2024-04-13 23:52:04 +0800

    defining component props tpyes

--- TypeScript_with_React_Essentials/src/App.tsx
+++ TypeScript_with_React_Essentials/src/App.tsx
@@ -1,3 +1,12 @@
+import CourseGoal from "./components/CourseGoal";
+
 export default function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+    <main>
+      <CourseGoal
+        title="learn react + ts"
+        description="learn react with typescript"
+      />
+    </main>
+  );
 }
---
+++ TypeScript_with_React_Essentials/src/components/CourseGoal.tsx
@@ -0,0 +1,19 @@
+const CourseGoal = ({
+  title,
+  description,
+}: {
+  title: string;
+  description: string;
+}) => {
+  return (
+    <article>
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </div>
+      <button>Delete</button>
+    </article>
+  );
+};
+
+export default CourseGoal;
