ae74d9aad6af2c33db495c29970c2bc5bbcc01be
yichien <kevinyichienlee@gmail.com>
2024-04-18 00:44:23 +0800

    Accessing Context Type-Safe With a Custom Hook

--- Advanced State Management/src/components/Header.tsx
+++ Advanced State Management/src/components/Header.tsx
@@ -1,11 +1,14 @@
-import Button from './UI/Button.tsx';
+import { useTimerContext } from "../store/timers-context.tsx";
+import Button from "./UI/Button.tsx";
 
 export default function Header() {
+  const timerCtx = useTimerContext();
+
   return (
     <header>
       <h1>ReactTimer</h1>
 
-      <Button>Stop Timers</Button>
+      <Button>{timerCtx.isRunning ? "Stop" : "Start"} Timers</Button>
     </header>
   );
 }
--- Advanced State Management/src/store/timers-context.tsx
+++ Advanced State Management/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, createContext } from "react";
+import { type ReactNode, createContext, useContext } from "react";
 
 type Timer = {
   name: string;
@@ -18,6 +18,18 @@ type TimersContextValue = TimersState & {
 
 // 因為一開始沒有符合TimersContextValue的Type值，所以先設定為null type
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+// 因為可能為null，所以要加上!，確保不會為null
+// 直接做一個hook，方便使用，不然每次用context都要加! 或是 判斷是否為null
+export function useTimerContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error("useTimerContext is null - that should not be the case!");
+  }
+
+  return timersCtx;
+}
 
 type TimersContextProviderProps = {
   children: ReactNode;
