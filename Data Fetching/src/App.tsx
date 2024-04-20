492cd958cdd710a1b5fae78665eff3362bf8c204
yichien <kevinyichienlee@gmail.com>
2024-04-20 23:54:14 +0800

    Fetching & Transforming Data

--- Data Fetching/src/App.tsx
+++ Data Fetching/src/App.tsx
@@ -1,5 +1,53 @@
+import BlogPosts, { type BlogPost } from "./components/BlogPosts";
+import { get } from "./util/http";
+import { type ReactNode, useEffect, useState } from "react";
+import fetchingImg from "./assets/data-fetching.png";
+
+type RawDataBlogPost = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const data = (await get(
+        "https://jsonplaceholder.typicode.com/posts"
+      )) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map((rawPost) => {
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body,
+        };
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />;
+  }
+
+  return (
+    <main>
+      <img
+        src={fetchingImg}
+        alt="An abstract image depicting a data fectching process"
+      />
+      {content}
+    </main>
+  );
 }
 
 export default App;
