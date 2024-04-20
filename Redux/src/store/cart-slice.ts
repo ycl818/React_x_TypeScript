cac0312d9be29a2626c358046a8eefae06d523c7
yichien <kevinyichienlee@gmail.com>
2024-04-21 00:39:37 +0800

    add logic into cartSlice's reducer functions

--- Redux/src/store/cart-slice.ts
+++ Redux/src/store/cart-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { type PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 type CartItem = {
   id: string;
@@ -19,7 +19,26 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart(){},
-    removeFromCart(){},
+    addToCart(
+      state, 
+      action: PayloadAction<{id: string; title: string; price:number}>
+    ){
+      const itemIndex = state.items.findIndex((item) => item.id === action.payload.id);
+
+      if(itemIndex >= 0){
+        state.items[itemIndex].quantity++;
+      } else {
+        state.items.push({...action.payload, quantity: 1});
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>){
+
+      const itemIndex = state.items.findIndex((item) => item.id === action.payload);
+
+      if(state.items[itemIndex].quantity === 1){
+      } else {
+        state.items[itemIndex].quantity--;
+      }
+    },
   }
 });
