fafee5eeb3b5c278e747841c821fad8bade8f792
yichien <kevinyichienlee@gmail.com>
2024-04-22 23:48:52 +0800

    add MainHeader component

--- Challenge-Book A Session/src/components/Navigation/MainHeader.tsx
+++ Challenge-Book A Session/src/components/Navigation/MainHeader.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import Button from "../Button";
 
 const MainHeader = () => {
-  return <div>MainHeader</div>;
+  return (
+    <>
+      <header id="main-header">
+        <h1>ReactMentoring</h1>
+        <nav>
+          <ul>
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active" : "")}
+                end
+              >
+                Our Mission
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/sessions"
+                className={({ isActive }) => (isActive ? "active" : "")}
+              >
+                Browse Sessions
+              </NavLink>
+            </li>
+            <li>
+              <Button onClick={() => {}}>Upcoming Sessions</Button>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 };
 
 export default MainHeader;
--- Challenge-Book A Session/src/pages/Root.tsx
+++ Challenge-Book A Session/src/pages/Root.tsx
@@ -1,9 +1,11 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import MainHeader from "../components/Navigation/MainHeader";
 
 export default function Root() {
   return (
     <>
       {/* Todo: Add Header */}
+      <MainHeader />
       <Outlet />
     </>
   );
