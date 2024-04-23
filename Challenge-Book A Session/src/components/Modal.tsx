8d45c6b76cbcfbc6715ac26c84b3a8859e143689
yichien <kevinyichienlee@gmail.com>
2024-04-24 00:23:14 +0800

    add comment in Modal component

--- Challenge-Book A Session/src/components/Modal.tsx
+++ Challenge-Book A Session/src/components/Modal.tsx
@@ -1,9 +1,13 @@
 import { forwardRef, useImperativeHandle, useRef, type ReactNode } from "react";
 import { createPortal } from "react-dom";
 
+// This type is used with `forwardRef` to ensure that the `Modal` component can be used
+//with `useImperativeHandle` to expose a `open` method
 type ModalProps = {
   children: ReactNode;
   onClose: () => void;
+  // The onClose function prop is used to propagate the default "close" event that can be triggered by <dialog>
+  //(for example, when the ESC key is pressed)
 };
 
 export type ModalHandle = {
@@ -14,16 +18,17 @@ const Modal = forwardRef<ModalHandle, ModalProps>(
   ({ children, onClose }, ref) => {
     const dialog = useRef<HTMLDialogElement>(null);
 
+    // // useImperativeHandle is used to expose the `open` method to other components
     useImperativeHandle(ref, () => {
       return {
         open: () => {
           if (dialog.current) {
-            dialog.current.showModal();
+            dialog.current.showModal(); // showModal() is a built-in method available on the <dialog> element
           }
         },
       };
     });
-
+    // Below, the onClose prop is set on <dialog> to handle the case that the user closes the modal via a built-in mechanism (ESC key)
     return createPortal(
       <dialog ref={dialog} onClose={onClose} className="modal">
         {children}
